ce42f7c01607d51c485a9b40bc8d1b06d01e0566
Ahmed Amine Chakroun <chakroun.a.a@gmail.com>
2019-02-11 06:23:25 -0800

    commit number 1

---
+++ build/precache-manifest.4705c50352ea1899bcc45bf64e2d613f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5287d54952a41e5c8164",
+    "url": "/static/css/main.75718cec.chunk.css"
+  },
+  {
+    "revision": "5287d54952a41e5c8164",
+    "url": "/static/js/main.5287d549.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "d16a266844ec01aafc8a",
+    "url": "/static/js/2.d16a2668.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "9d08a8111757427e594aa7b598ef2fdc",
+    "url": "/index.html"
+  }
+];
