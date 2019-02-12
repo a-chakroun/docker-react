949a94c2e906f73bbe25e9ab743d25f524ba7194
Ahmed Amine Chakroun <chakroun.a.a@gmail.com>
2019-02-11 23:25:14 -0800

    testing morning master

--- src/App.js
+++ src/App.js
@@ -17,8 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-           docker kiber
-          </a>
+master   branch      </a>
         </header>
       </div>
     );
